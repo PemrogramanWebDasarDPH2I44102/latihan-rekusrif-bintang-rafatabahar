6cb4cd1c764e0845150eae673933d2f8994110f6
rafatabahar <39862459+rafatabahar@users.noreply.github.com>
2018-09-13 07:56:57 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -8,3 +8,58 @@ function rekursif($i){
 }
 rekursif(5);
 ?>
+
+
+/*===================================================================================*/
+
+<form class="" action="TA1_6701170104_Rafata Baharansyah.php" method="post">
+  Batas Piramida<input type="number" name="bts"><br>
+  <input type="submit" name="submit">
+</form>
+
+<?php
+
+  if (isset($_POST['submit'])) {
+      $batas = $_POST['bts'];
+      piramidaRekursif(0,$batas);
+  }
+
+  function piramidaRekursif($i = 0, $bts = 5){
+    echo "*";
+    $i++;
+    if ($i<$bts) {
+      piramidaRekursif($i,$bts);
+    }else {
+      echo "<br>";
+      $bts--;
+      if ($bts!=0) {
+          piramidaRekursif(0,$bts);
+      }
+    }
+  }
+
+echo "<br>";
+/*==========================================================================*/
+echo "<br>";
+
+  function piramidaRekursif2($i = 1, $bts = 1){
+    echo "$i";
+    $i++;
+    if ($i<=$bts) {
+      piramidaRekursif2($i,$bts);
+    }else {
+      echo "<br>";
+      $bts++;
+      if ($bts<=5) {
+          piramidaRekursif2(1,$bts);
+      }
+    }
+  }
+
+piramidarekursif2();
+
+?>
+
+
+
+
